be60e49aa8537e9c6f6fc0c1e8b94a531dd4ccfc
cfxmj2014 <444952680@qq.com>
2016-04-01 23:43:29 +0800

    init project

---
+++ src/main/java/cn/internship/action/HelloAction.java
@@ -0,0 +1,12 @@
+package cn.internship.action;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class HelloAction extends ActionSupport{
+
+	@Override
+	public String execute() throws Exception {
+		return super.execute();
+	}
+	
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,6 @@
+<html>
+<body>
+<h2>Hello World!</h2>
+<a href="helloAction">action test</a>
+</body>
+</html>
