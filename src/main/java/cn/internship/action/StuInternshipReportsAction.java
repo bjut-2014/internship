4ce9ea7f160ca5494017122c2b91032cd4cc3e79
dreamlate <menghanmhmh@gmail.com>
2016-04-10 20:37:33 +0800

    添加实习报告模块

--- src/main/java/cn/internship/action/StuInternshipReportsAction.java
+++ src/main/java/cn/internship/action/StuInternshipReportsAction.java
@@ -1,10 +1,60 @@
 package cn.internship.action;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.struts2.interceptor.ServletRequestAware;
+import org.apache.struts2.interceptor.ServletResponseAware;
+
+import cn.internship.entity.InternshipReport;
+import cn.internship.entity.Student;
+import cn.internship.service.InternshipReportService;
+
 import com.opensymphony.xwork2.ActionSupport;
 
-public class StuInternshipReportsAction extends ActionSupport {
-    @Override
-    public String execute() throws Exception {
-        return super.execute();
-    }
+/**
+ * 实习报告页面
+ * @author MengHan
+ */
+public class StuInternshipReportsAction extends ActionSupport  implements ServletRequestAware, ServletResponseAware {
+	private HttpServletRequest request;
+	private HttpServletResponse response;
+	
+	private InternshipReportService internshipReportService;
+	
+	@Override
+	public String execute() throws Exception {
+		//获得当前session下的学生
+		Student student = (Student) request.getSession().getAttribute("currentUser");
+		InternshipReport internshipReport = internshipReportService.get(student.getStudentId());
+		if(internshipReport == null){
+			return INPUT;
+		}
+		request.getSession().setAttribute("internshipReport", internshipReport);
+		return super.execute();
+	}
+	
+	
+	//-----------------------get与set方法------------------------
+	
+	@Override
+	public void setServletResponse(HttpServletResponse response) {
+		this.response = response;
+	}
+	@Override
+	public void setServletRequest(HttpServletRequest request) {
+		this.request = request;
+	}
+
+
+	public InternshipReportService getInternshipReportService() {
+		return internshipReportService;
+	}
+
+
+	public void setInternshipReportService(
+			InternshipReportService internshipReportService) {
+		this.internshipReportService = internshipReportService;
+	}
+
 }
