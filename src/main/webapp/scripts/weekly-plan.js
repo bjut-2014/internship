7bae8135938f2f1d0c05408312ba3a3edfc81c3c
dreamlate <menghanmhmh@gmail.com>
2016-04-11 00:21:33 +0800

    修改表单提交的bug

--- src/main/webapp/scripts/weekly-plan.js
+++ src/main/webapp/scripts/weekly-plan.js
@@ -3,7 +3,8 @@
  */
 var $weeklyPlanDel = $('.weekly-plan-del'),
     $weeklyPlanDelConfirm = $('.weekly-plan-del-confirm'),
-    $weeklyPlanEdit = $('.weekly-plan-edit');
+    $weeklyPlanEdit = $('.weekly-plan-edit'),
+    $weeklyPlanEditConfirm = $('.weekly-plan-update-confirm');
 
 $weeklyPlanDel.on('click', function() {
     var id = $(this).attr('data-id');
@@ -26,7 +27,9 @@ $weeklyPlanDelConfirm.on('click', function() {
 });
 $weeklyPlanEdit.on('click', function() {
     var id = $(this).attr('data-id');
-
+    
+    $('#WeeklyPlanModify').attr('data-id', id);
+    
     $.ajax({
         url: 'weekly-plan-edit',
         type: 'POST',
@@ -39,6 +42,26 @@ $weeklyPlanEdit.on('click', function() {
             $('.plan-title').val(data.weeklyplanTitle);
             $('.plan-content').val(data.weeklyplanContent);
             $('.plan-date').val(data.weeklyplanDate);
+        }
+    })
+});
+$weeklyPlanEditConfirm.on('click',function(){
+	var id = $('#WeeklyPlanModify').attr('data-id');
+//	var title = $('#updateTitle')val();
+//    var content = $('#updateContent')val();
+    
+	$.ajax({
+        url: 'weekly-plan-update',
+        type: 'POST',
+        dataType: 'JSON',
+        data: {
+        	updateId: id,
+        	updateTitle : $('.plan-title').val(),
+        	updateContent : $('.plan-content').val()
+        },
+        success: function(data) {
+//            window.location.reload();
+        	url:weekly-plan
         }
     })
 });
