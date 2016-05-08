56b8c7500e1bb84b1f90fcad886f18b4a81daa67
dreamlate <menghanmhmh@gmail.com>
2016-05-08 14:26:02 +0800

    build a new branch

---
+++ src/main/java/cn/internship/action/AdminAction.java
@@ -0,0 +1,17 @@
+package cn.internship.action;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+/**
+ * 招聘、公告、轮播图的相关请求与操作
+ * @author MengHan
+ *
+ */
+public class AdminAction extends ActionSupport{
+
+	@Override
+	public String execute() throws Exception {
+		return super.execute();
+	}
+	
+}
